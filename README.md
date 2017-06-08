8e60f2314459cf124ed9e241ef0dc3c59f27a97f
Jean-Francois <jfrancois.lai@gmail.com>
2017-06-08 21:53:05 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Mocha / Chai POC
+ 
+ #### Mocha npm runner
+  Run with ```npm test```. 
+   
+   **Observations**:
+   - Run with node
+   - Config via command line arguments
+
---
+++ src/test/app_test.js
@@ -0,0 +1,14 @@
+/* eslint-env mocha */
+const expect = require('chai').expect
+
+describe('normalizewewe weweweweeData', function () {
+  it('accepts golden path data', function () {
+    expect('Maria').to.equal('Maria')
+  })
+
+  it('accepts 323232 path data', function () {
+    expect('ewewew').to.equal('Maria')
+
+    expect('Maria').to.equal('Maria')
+  })
+})
